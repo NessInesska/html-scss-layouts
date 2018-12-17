f6c073884c60c13f91e2fe4811b08e56d23655ee
Inessa Borodich <inesska@MacBook-Air-Inessa.local>
2018-12-17 23:58:18 +0400

    Centering element vertically with transform

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CenteringElementComponent } from './centering-element/centering-element.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CenteringElementComponent,
   ],
   imports: [
     BrowserModule
---
+++ src/app/centering-element/centering-element.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-centering-element',
+  templateUrl: './centering-element.component.html',
+  styleUrls: ['./centering-element.component.scss']
+})
+export class CenteringElementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
