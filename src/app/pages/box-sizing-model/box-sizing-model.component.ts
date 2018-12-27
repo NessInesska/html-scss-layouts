69a6b83b21d2b07301abddc869059087107b3f96
Inessa Borodich <iborodich@gds000387-nb.carina.griddynamics.net>
2018-12-27 14:16:12 +0400

    Create box-sizing component

---
+++ src/app/pages/box-sizing-model/box-sizing-model.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-box-sizing-model',
+  templateUrl: './box-sizing-model.component.html',
+  styleUrls: ['./box-sizing-model.component.scss']
+})
+export class BoxSizingModelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
